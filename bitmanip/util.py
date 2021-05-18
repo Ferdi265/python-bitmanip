c96a375019b874e3cd17be69c968c16148e06fa9
Ferdinand Bachmann <theferdi265@gmail.com>
2021-05-18 18:26:50 +0200

    bitmanip/util: add sign extend utility, make functions little endian

--- bitmanip/util.py
+++ bitmanip/util.py
@@ -31,23 +31,32 @@ def bind2(f: Callable[[int, int], int], arg2: int) -> Callable[[int], int]:
 def bind23(f: Callable[[int, int, int], int], arg2: int, arg3: int) -> Callable[[int], int]:
     return lambda arg1: f(arg1, arg2, arg3)
 
+def mask(bits: int) -> int:
+    return (1 << bits) - 1
+
 def bits(n: int, bits: int) -> int:
-    return n & (2 ** bits - 1)
+    return n & mask(bits)
+
+def sext(n: int, b1: int, b2: int) -> int:
+    n = bits(n, b1)
+    sign_bit = n & (1 << (b1 - 1))
+    sext_mask = mask(b2 - b1) << b1
+    return sext_mask | n
 
 def split8(n: int) -> Tuple[int, int]:
-    return (bits(bits(n, 16) >> 8, 8), bits(n, 8))
+    return (bits(n, 8), bits(bits(n, 16) >> 8, 8))
 
-def cat8(h: int, l: int) -> int:
+def cat8(l: int, h: int) -> int:
     return bits((bits(h, 8) << 8) | bits(l, 8), 16)
 
 def split16(n: int) -> Tuple[int, int]:
-    return (bits(bits(n, 32) >> 16, 16), bits(n, 16))
+    return (bits(n, 16), bits(bits(n, 32) >> 16, 16))
 
-def cat16(h: int, l: int) -> int:
+def cat16(l: int, h: int) -> int:
     return bits((bits(h, 16) << 16) | bits(l, 16), 32)
 
 def split32(n: int) -> Tuple[int, int]:
-    return (bits(bits(n, 64) >> 32, 32), bits(n, 32))
+    return (bits(n, 32), bits(bits(n, 64) >> 32, 32))
 
-def cat32(h: int, l: int) -> int:
+def cat32(l: int, h: int) -> int:
     return bits((bits(h, 32) << 32) | bits(l, 32), 64)
