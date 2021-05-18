5c0a2278300a11338773c235e4ca7fe98499e940
Ferdinand Bachmann <theferdi265@gmail.com>
2021-05-18 17:39:59 +0200

    initial commit

---
+++ bitmanip/__init__.py
@@ -0,0 +1,4 @@
+from .version import __version__
+from .util import *
+from .base import *
+from .bitmanip import *
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+with open("bitmanip/version.py", "r") as f:
+    exec(f.read(), globals())
+
+setup(
+    name = "bitmanip",
+    description = "bit manipulation functions based on RISC-V BitManip extension",
+    version = __version__,
+    author = "Ferdinand Bachmann",
+    author_email = "theferdi265@gmail.com",
+    packages = ["bitmanip"],
+    entry_points = {},
+    python_requires = ">=3.5",
+    install_requires = requirements
+)
